f875636de2e26bcc16c7ea3415c1d9e3ed5bd43f
中川拓哉 <takuya.nakagawa@ietty.co.jp>
2023-07-30 16:33:03 +0900

    Initial Commit

---
+++ WashCarInformations/WashCarInformationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashCarInformationsApp.swift
+//  WashCarInformations
+//
+//  Created by nakagawatakuya on 2023/07/30.
+//
+
+import SwiftUI
+
+@main
+struct WashCarInformationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
